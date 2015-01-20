802c4181b7d5fdf5603dfbdea79ed806bf695c8d
GregPelfrey <greg.pelfrey@gmail.com>
2015-01-20 16:23:28 -0500

    Finished

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -18,7 +18,22 @@ class CarsController < ApplicationController
 	end
 
 	def show
-		@car = Car.find params [:id]
+		@car = Car.find params[:id]
 	end
 
+	def destroy
+		@car = Car.find params[:id]
+		@car.delete
+		redirect_to root_path
+	end
+
+	def edit
+		@edit_car = Car.find params[:id]
+	end
+
+	def update
+		@car = Car.find params[:id]
+    	@car.update_attributes car_params
+    	redirect_to root_path
+	end
 end
