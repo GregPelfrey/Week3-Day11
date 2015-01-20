036db33a0ca65e36a807f49d5403118428b06995
GregPelfrey <greg.pelfrey@gmail.com>
2015-01-20 15:38:19 -0500

    338

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,24 @@
 class CarsController < ApplicationController
+	def new
+		@new_car = Car.new
+	end
+
+	def create
+		@car = Car.create car_params
+		redirect_to root_path
+	end
+
+	def car_params
+		params.require(:car).permit(
+			:make,
+			:model,
+			:year,
+			:color
+			)
+	end
+
+	def show
+		@car = Car.find params [:id]
+	end
+
 end
